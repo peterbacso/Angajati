b530408badab4413d898a837e41e081d3c64490e
AzureAD\Peter-TamasBacso <peter.bacso@principal33.com>
2021-04-26 10:16:15 +0300

    added searchFunction and seed data.

--- main.js
+++ main.js
@@ -81,3 +81,23 @@ function checkImage(numePoza) {
     if(numePoza.indexOf('.jpg') >= 0 || numePoza.indexOf('.jpeg') >= 0 || numePoza.indexOf('.png') >= 0) return true;
     return false;
 }
+
+// Search
+function searchFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("angajati");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }       
+    }
+}
